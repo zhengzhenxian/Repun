ac28513ac7fdd1e29b001d9a7553434d9069e1b9
zhengzhenxian <920596906@qq.com>
2021-09-15 13:42:56 +0800

    calculate phase set in vcf file

---
+++ src/cal_phase_set.py
@@ -0,0 +1,129 @@
+import shlex
+import os
+import sys
+from os.path import isfile, abspath
+from sys import exit, stderr
+from subprocess import check_output, PIPE, Popen
+import argparse
+import shlex
+from subprocess import PIPE
+import subprocess
+from collections import defaultdict
+
+from argparse import ArgumentParser, SUPPRESS
+from collections import Counter, defaultdict
+
+
+def subprocess_popen(args, stdin=None, stdout=PIPE, stderr=stderr, bufsize=8388608):
+    return Popen(args, stdin=stdin, stdout=stdout, stderr=stderr, bufsize=bufsize, universal_newlines=True)
+
+def phase(args):
+    contig_name =args.ctgName
+    vcf_fn = args.vcf_fn
+    variant_dict = defaultdict(set)
+    unzip_process = subprocess_popen(shlex.split("gzip -fdc %s" % (vcf_fn)))
+    for row in unzip_process.stdout:
+        row = row.rstrip()
+        if row[0] == '#':
+            continue
+        columns = row.strip().split('\t')
+
+        ctg_name = columns[0]
+        if contig_name and contig_name != ctg_name:
+            continue
+        pos = int(columns[1])
+        ref_base = columns[3]
+        alt_base = columns[4]
+        genotype_info = columns[9].split(':')
+        genotype, phase_set = genotype_info[0], genotype_info[-1]
+        if '|' not in genotype:  # unphasable
+            continue
+
+
+        variant_dict[phase_set].add(pos)
+    total_phase_set = len(variant_dict.keys())
+    num_list = [len(item) for item in variant_dict.values()]
+    max_num, mean_num, min_num = max(num_list), int(sum(num_list)/total_phase_set), min(num_list)
+    print ("[INFO] {}: total_phase_set:{}, max:{}, mean:{}, min:{}".format(contig_name, total_phase_set, max_num, mean_num, min_num))
+
+
+def main():
+    parser = ArgumentParser(description="Generate variant candidate tensors using phased full-alignment")
+
+    parser.add_argument('--platform', type=str, default='ont',
+                        help="Sequencing platform of the input. Options: 'ont,hifi,ilmn', default: %(default)s")
+
+    parser.add_argument('--bam_fn', type=str, default="input.bam",  # required=True,
+                        help="Sorted BAM file input, required")
+
+    parser.add_argument('--ref_fn', type=str, default="ref.fa",  # required=True,
+                        help="Reference fasta file input, required")
+
+    parser.add_argument('--tensor_can_fn', type=str, default="PIPE",
+                        help="Tensor output, stdout by default, default: %(default)s")
+
+    parser.add_argument('--vcf_fn', type=str, default=None,
+                        help="Candidate sites VCF file input, if provided, variants will only be called at the sites in the VCF file,  default: %(default)s")
+
+    parser.add_argument('--min_af', type=float, default=0.08,
+                        help="Minimum allele frequency for both SNP and Indel for a site to be considered as a condidate site, default: %(default)f")
+
+    parser.add_argument('--ctgName', type=str, default=None,
+                        help="The name of sequence to be processed, required if --bed_fn is not defined")
+
+    parser.add_argument('--ctgStart', type=int, default=None,
+                        help="The 1-based starting position of the sequence to be processed, optional, will process the whole --ctgName if not set")
+
+    parser.add_argument('--ctgEnd', type=int, default=None,
+                        help="The 1-based inclusive ending position of the sequence to be processed, optional, will process the whole --ctgName if not set")
+
+    parser.add_argument('--bed_fn', type=str, default=None,
+                        help="Call variant only in the provided regions. Will take an intersection if --ctgName and/or (--ctgStart, --ctgEnd) are set")
+
+
+    parser.add_argument('--sampleName', type=str, default="SAMPLE",
+                        help="Define the sample name to be shown in the GVCF file")
+
+    parser.add_argument('--samtools', type=str, default="samtools",
+                        help="Path to the 'samtools', samtools version >= 1.10 is required. default: %(default)s")
+
+    # options for advanced users
+    parser.add_argument('--minCoverage', type=float, default=2,
+                        help="EXPERIMENTAL: Minimum coverage required to call a variant, default: %(default)f")
+
+    parser.add_argument('--minMQ', type=int, default=5,
+                        help="EXPERIMENTAL: If set, reads with mapping quality with <$minMQ are filtered, default: %(default)d")
+
+    parser.add_argument('--minBQ', type=int, default=0,
+                        help="EXPERIMENTAL: If set, bases with base quality with <$minBQ are filtered, default: %(default)d")
+
+    parser.add_argument('--max_depth', type=int, default=144,
+                        help="EXPERIMENTAL: Maximum full alignment depth to be processed. default: %(default)s")
+
+    # options for debug purpose
+    parser.add_argument('--phasing_info_in_bam', action='store_true',
+                        help="DEBUG: Skip phasing and use the phasing info provided in the input BAM (HP tag), default: False")
+
+    parser.add_argument('--extend_bed', type=str, default=None,
+                        help="DEBUG: Extend the regions in the --bed_fn by a few bp for tensor creation, default extend 16bp")
+
+    parser.add_argument('--indel_fn', type=str, default=None,
+                        help="DEBUG: Output all alternative indel cigar for debug purpose")
+
+    parser.add_argument('--base_err', default=0.001, type=float,
+                        help='DEBUG: Estimated base error rate in gvcf option, default: %(default)f')
+
+    parser.add_argument('--gq_bin_size', default=5, type=int,
+                        help='DEBUG: Default gq bin size for merge non-variant block in gvcf option, default: %(default)d')
+
+    parser.add_argument('--bp_resolution', action='store_true',
+                        help="DEBUG: Enable bp resolution for GVCF, default: disabled")
+
+
+    args = parser.parse_args()
+
+    phase(args)
+
+
+if __name__ == "__main__":
+    main()
